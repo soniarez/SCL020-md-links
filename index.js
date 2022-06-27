dd0dcf154c0249a38b31dc7ec73f0e6f9dd08a90
soniarez <101676781+soniarez@users.noreply.github.com>
2022-06-27 17:55:04 -0400

    reading a simple file

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+var file = require('file-system');
+var fs = require('fs');
+ 
+// file.readFile === fs.readFile
+
+fs.readFile('demo1.md', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
