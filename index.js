6dc698ff5c0ed37f8349494cce317d8846d877b3
soniarez <101676781+soniarez@users.noreply.github.com>
2022-06-30 12:42:35 -0400

    consologing array of links in httpreq func

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // };
 const fs = require("fs");
 const { readFileSync } = require("fs");
-const markdownLinkExtractor = require('markdown-link-extractor');
+const markdownLinkExtractor = require("markdown-link-extractor");
 const path = require("path");
 const axios = require("axios").default;
 
@@ -13,7 +13,7 @@ const readingFile = (filename) => {
     const markdown = readFileSync(filename, { encoding: "utf8" });
     const links = markdownLinkExtractor(markdown);
     const linksArr = [];
-
+    //console.log(links);
     links.forEach((link) => linksArr.push(link));
     //console.log(linksArr);
     return linksArr;
@@ -28,50 +28,46 @@ const httpReq = (filename) => {
   const files = readingFile(filename);
   const fetchingLinks = [];
 
-  const linkStatus = files.map((url) => {
-    //console.log(url);
-    return new Promise((resolve, reject) => {
-      const ax = axios.get(url)
-        .then((response) => {
-          const successLinks = {
+  files.forEach((url) => {
+    axios.get(url)
+      .then((response) => {
+        const successLinks = {
+          link: url,
+          status: response.status,
+          statusText: response.statusText,
+        };
+        //console.log(successLinks, "hola estoy aca");
+        fetchingLinks.push(successLinks);
+        return successLinks;
+        //console.log(fetchingLinks.length, files.length);
+      })
+      .catch((error) => {
+        if (error.response) {
+          const failLinks = {
             link: url,
-            status: response.status,
-            statusText: response.statusText,
+            status: error.response.status,
+            statusText: error.response.statusText,
           };
-          console.log(successLinks);
-          fetchingLinks.push(successLinks);
-          return successLinks;
-        })
-        .catch((error) => {
-          if (error.response) {
-            const failLinks = {
-              link: url,
-              status: error.response.status,
-              statusText: error.response.statusText,
-            };
-            console.log(failLinks);
-            fetchingLinks.push(failLinks);
-            return failLinks;
-          }
-        });
-        resolve(ax);
-        reject((error) => console.log("promise failed"), error);
-    });
+          fetchingLinks.push(failLinks);
+          return failLinks;
+        }
+      })
+      .finally(() => {
+        if (fetchingLinks.length === files.length) {
+          console.log(fetchingLinks);
+         }
+      });
   });
-  Promise.all(linkStatus);
-   return fetchingLinks;
 };
-console.log(httpReq("demo1.md"));
+httpReq("demo1.md");
 
 // Getting md files from a directory
 const readingDir = (__dirname) => {
   files = fs.readdirSync(__dirname);
   console.log("/Filenames with the .md extension:");
 
-  files.forEach(file => {
-    if (path.extname(file) == ".md")
-      console.log(file);
-  })
-}
+  files.forEach((file) => {
+    if (path.extname(file) == ".md") console.log(file);
+  });
+};
 readingDir("C:/Users/carolina.briones/Desktop/COPY-SCL020-memory-match");
-
