76ff3a9a93b00d6bfc2d02b01b93417b8d0c343a
soniarez <101676781+soniarez@users.noreply.github.com>
2022-07-01 15:49:52 -0400

    reading a directory

--- index.js
+++ index.js
@@ -2,45 +2,42 @@
 //   // ...
 // };
 const fs = require("fs");
-const { readFileSync } = require("fs");
+//const { readFileSync } = require("fs");
 const marked = require("marked");
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 const path = require("path");
 const axios = require("axios").default;
 
-
-// Extracting Links
+// Extracting Links and text from link
 const extractLinks = (filename) => {
   try {
     const data = fs.readFileSync(filename, "utf8");
     const dataHtml = marked.parse(data); // using marked to transform md to html
     const $ = cheerio.load(dataHtml); // using cheerio to extract <a> tags
-    const linksObjects = $('a');  // this is a mass object, not an array
+    const linksObjects = $("a"); // this is a mass object, not an array
     //console.log(linksObjects);
 
     const linksArr = [];
     linksObjects.each((index, link) => {
       linksArr.push({
-      text: $(link).text(),
-      href: $(link).attr('href')
-      })  
+        text: $(link).text(),
+        href: $(link).attr("href"),
+      });
     });
-    //console.log(linksArr, "linksArr in extractLink func");
+    // console.log(linksArr, "linksArr in extractLink func");
     return linksArr;
   } catch (err) {
     console.error(err);
   }
 };
-extractLinks("demo1.md");
-
+extractLinks("./demo/demo1.md");
 
 // Http Request - Checking Link Status
- const validateStatus = (filename) => {
+const validateStatus = (filename) => {
   const files = extractLinks(filename);
   //console.log(files, "files in validateStatus");
 
-  const fetchingLinks = []; 
-
+  const fetchingLinks = [];
   files.forEach((urlObj) => {
     const url = urlObj.href;
     const linkText = urlObj.text;
@@ -71,20 +68,24 @@ extractLinks("demo1.md");
       })
       .finally(() => {
         if (fetchingLinks.length === files.length) {
-           console.log(fetchingLinks);
+          console.log(fetchingLinks);
         }
       });
-  }); 
-};
-validateStatus("demo1.md");  
-
-// Getting md files from a directory
-const readingDir = (__dirname) => {
-  files = fs.readdirSync(__dirname);
-  console.log("/Filenames with the .md extension:");
-
-  files.forEach((file) => {
-    if (path.extname(file) == ".md") console.log(file);
   });
 };
-readingDir("C:/Users/carolina.briones/Desktop/COPY-SCL020-memory-match");
+validateStatus("./demo/demo1.md");
+
+// Getting md files from a directory
+const readDir = (__dirname) => {
+  try {
+    files = fs.readdirSync(__dirname);
+    console.log("/Filenames with the .md extension:");
+
+    files.forEach((file) => {
+      if (path.extname(file) == ".md") console.log(file);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+readDir("./demo");
