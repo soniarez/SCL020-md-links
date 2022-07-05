77e9c3025b6087cb3aa35b51e0decf366b3bb7c6
soniarez <101676781+soniarez@users.noreply.github.com>
2022-07-05 15:01:35 -0400

    promise all v1 with multiple bugs

--- index.js
+++ index.js
@@ -1,125 +1,24 @@
+/* const path = require("path");
 const fs = require("fs");
-const marked = require("marked");
-const cheerio = require("cheerio");
-const path = require("path");
-const axios = require("axios").default;
-const process = require("process");
+const { validateStatus, getAllFiles } = require("./mdlinks.js");
 
-// Extracting Links and text from link
-const extractLinks = (filename) => {
-  try {
-    const data = fs.readFileSync(filename, "utf8");
-    const dataHtml = marked.parse(data); // using marked to transform md to html
-    const $ = cheerio.load(dataHtml); // using cheerio to extract <a> tags
-    const linksObjects = $("a"); // this is a mass object, not an array
-    //console.log(linksObjects in extractLinks func);
-
-    const linksObjArr = [];
-    linksObjects.each((index, link) => {
-      linksObjArr.push({
-        text: $(link).text(),
-        href: $(link).attr("href"),
-      });
+const mdLinks = (userFilePath) => {
+  
+  if (fs.statSync(userFilePath).isDirectory()) {
+    
+    let filesArr = []; //Im getting all files from GetAllFiles (not just .md)
+    getAllFiles(userFilePath, filesArr);
+    filesArr.forEach((file) => {
+      const validatingStatus = validateStatus(file);
+      if (path.extname(file) === ".md") {
+        console.log(validatingStatus);
+        return validateStatus;
+      }
     });
-    //console.log(linksObjArr, "linksArr in extractLink func");
-    return linksObjArr;
-  } catch (err) {
-    console.error(err);
+  } else {
+    const isFile = validateStatus(userFilePath);
+    console.log(isFile);
+    return isFile;
   }
-};
-//extractLinks("./demo/subDemo/subFile.md");
-
-
-// Http Request - Checking Link Status
-const validateStatus = (filename, options) => {
-  const files = extractLinks(filename);
-
-  const fetchingLinks = [];
-
-  files.map((urlObj) => {
-    const url = urlObj.href;
-    const linkText = urlObj.text;
-
-    // Object containining basic info if user does not use options
-    let baseDataLink = {
-      href: url,
-      text: linkText,
-      file: filename,
-    }
-
-    // http request with axios
-    axios
-      .get(url)
-      .then((response) => {
-        if (process.argv[2] === "--validate") {
-          baseDataLink.status =  response.status,
-          baseDataLink.statusText = response.statusText
-          fetchingLinks.push(baseDataLink);
-          //console.log(fetchingLinks.length, files.length);
-        }  else {
-          fetchingLinks.push(baseDataLink);
-        } 
-        return baseDataLink;
-      })
-      .catch((error) => {
-        if (error.response) {
-          if (process.argv[2] === "--validate") {
-            baseDataLink.status = error.response.status
-            baseDataLink.statusText = error.response.statusText
-          };
-          fetchingLinks.push(baseDataLink);
-          return baseDataLink;
-        };
-      })
-      .finally(() => {
-       //console.log(fetchingLinks.length, files.length);
-        if (fetchingLinks.length === files.length) {
-          // Checking for unique links  --> PREGUNTAR MAURO ESTO
-        const uniqueArr = fetchingLinks.filter((value, index, self) => {
-          return self.findIndex(v => v.href === value.href) === index;
-        })
-        //console.log(uniqueArr, "soy uniqueArr"); 
-        console.log(fetchingLinks); 
-        
-         if (process.argv[2] === "--stats") {
-          const stats  = {
-            total: fetchingLinks.length,
-            unique: uniqueArr.length
-          }
-          console.log(stats);
-          return stats;
-         }
-        }
-        return fetchingLinks;
-      });
-  });
-};
-//validateStatus("./demo/subDemo/subFile.md");
-
-
-// Getting all the files from directory - recursion - to be able to go through folders and subfolders
-const getAllFiles = (dirPath, filesArr) => {
-  const files = fs.readdirSync(dirPath);
-
-  filesArr = filesArr || [];
-
-  files.forEach((file) => {
-    // Going recursevely into each directory and subdirectory to add files into filesArr
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      // If it is a directory, the function recursevely call itself to get all files and subdirectories
-      filesArr = getAllFiles(dirPath + "/" + file, filesArr);
-    } else {
-      filesArr.push(path.join(__dirname, dirPath, "/", file));
-    }
-  });
-  return filesArr;
-};
-//const funt = getAllFiles("./demo");
-//console.log(funt, "estoy en getAllMdFiles func");
-
-
-module.exports = {
-  extractLinks,
-  validateStatus,
-  getAllFiles
-}
+}; 
+mdLinks("./demo/demo1.md"); */
--- mdlinks.js
+++ mdlinks.js
@@ -1,24 +1,71 @@
-const path = require("path");
 const fs = require("fs");
-const { validateStatus, getAllFiles } = require("./index.js");
+const marked = require("marked");
+const cheerio = require("cheerio");
+const path = require("path");
+const axios = require("axios").default;
 
-const mdLinks = (userFilePath, options) => {
-  
-  if (fs.statSync(userFilePath).isDirectory()) {
-    
-    let filesArr = []; //Im getting all files from GetAllFiles (not just .md)
-    getAllFiles(userFilePath, filesArr);
-    filesArr.forEach((file) => {
-      const validatingStatus = validateStatus(file);
-      if (path.extname(file) === ".md") {
-        console.log(validatingStatus);
-        return validateStatus;
-      }
+// Extracting Links and text from link
+const extractLinks = (filename) => {
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    const dataHtml = marked.parse(data); // using marked to transform md to html
+    const $ = cheerio.load(dataHtml); // using cheerio to extract <a> tags
+    const linksObjects = $("a"); // this is a mass object, not an array
+    //console.log(linksObjects in extractLinks func);
+
+    const linksObjArr = [];
+    linksObjects.each((index, link) => {
+      linksObjArr.push({
+        text: $(link).text(),
+        href: $(link).attr("href"),
+      });
     });
-  } else {
-    const isFile = validateStatus(userFilePath);
-    console.log(isFile);
-    return isFile;
+
+    return linksObjArr;
+  } catch (err) {
+    console.error(err);
   }
 };
-mdLinks("./demo/demo1.md");
+//extractLinks("./demo/subDemo/subFile.md");
+
+// Http Request - Checking Link Status
+const validateStatus = (filename) => {
+  const files = extractLinks(filename);
+
+  const fetchingLinks = [];
+
+  files.map((urlObj) => {
+    const url = urlObj.href;
+    const linkText = urlObj.text;
+
+    // http request with axios
+    const linkAx = axios
+      .get(url)
+      .then((response) => {
+        const dataLink = {
+          href: url,
+          text: linkText,
+          file: filename,
+          status: response.status,
+          statusText: response.statusText
+        };
+        return dataLink;
+      })
+      .catch((error) => {
+        if (error.response) {
+          const dataLink = {
+            href: url,
+            text: linkText,
+            file: filename,
+            status: error.response.status,
+            statusText: error.response.statusText
+          };
+          return dataLink;
+        }
+      });
+      fetchingLinks.push(linkAx);
+  });
+  return Promise.all(fetchingLinks);
+};
+validateStatus("./demo/demo1.md").then(console.log);
+
